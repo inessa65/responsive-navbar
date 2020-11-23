ce67c1cb94d04d776fae2111a5b7733d1e456d88
inessa65 <inessa65@yandex.by>
2020-11-23 08:55:49 +0300

    feat:add cards

--- script.js
+++ script.js
@@ -19,3 +19,50 @@ const navLinks =document.querySelectorAll('.nav-links li')
  })
 }
 navSlide()
+
+
+var oldScrollY = 0;
+var div = document.querySelector('.nav');
+
+window.addEventListener('scroll', function(){
+  var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+ 
+  var dY = scrolled - oldScrollY;
+  
+  if ( dY <= 0  ){
+    div.classList.add('nav-fixed');
+  }
+  // } else {
+  //   div.classList.remove('nav-fixed');
+  // }
+  
+  oldScrollY = scrolled;
+});
+
+
+//вывод карточек на страницу
+
+const sidebarCard= document.querySelector('.sidebar__cards')
+const request =new XMLHttpRequest();
+request.open("GET", "./sidebar.json");
+request.onload = ()=>{
+  cards =JSON.parse(request. response);
+  //console.log(cards)
+  let html ='';
+  cards.forEach(({img, title, text})=>{
+  html += `<a href="#">
+  <div class="sidebar__card">
+  <img src=${img}>
+  <h1 class="sidebar__title">${title}</h1>
+  <p>${text}</p>
+  </div>
+  </a>
+  `
+
+  sidebarCard.innerHTML =html;
+  console.log(html)
+  })
+
+
+};
+request.send()
